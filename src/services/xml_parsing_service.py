dda4cf35b8531722a177f3ebfc4b24714b17bec7
klhp <provoda51@gmail.com>
2026-03-18 14:16:23 +0300

    init

---
+++ src/services/xml_parsing_service.py
@@ -0,0 +1,10 @@
+# примеры вам, то есть конечная точка
+# все по своим файлам
+
+class XmlParsingService:
+    def __init__(self, files: dict | bytes):
+        ...
+
+class WebParsingService:
+    def __init__(self, url: str):
+        ...
