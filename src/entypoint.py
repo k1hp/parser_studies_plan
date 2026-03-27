a4634e2c32206448188544b3c3c1a459816f33f4
Umnica228 <mishatalalaev40@gmail.com>
2026-03-27 12:10:52 +0300

    Настроен вывод инфы

--- src/entypoint.py
+++ src/entypoint.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import List, Literal
 
 from fastapi import FastAPI, UploadFile, File, Depends, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src.dependencies import get_analyze_service
@@ -9,6 +10,16 @@ from src.services.analyze_service import AnalyzeService
 from src.utils import applogger
 
 app = FastAPI()
+
+# Добавляем CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 router = APIRouter(prefix="/api")
 
 REPORT_FORMATS = Literal["json", "html", "pdf"]
@@ -25,7 +36,6 @@ async def analyze(url: str, report_format: REPORT_FORMATS, file: UploadFile = Fi
         return analyze_service.analyze_one(url, content)
 
 
-
 @router.post("/compare/files/{report_format}")
 def analyze_many(report_format: REPORT_FORMATS, files: List[UploadFile]):
     ...
@@ -33,5 +43,4 @@ def analyze_many(report_format: REPORT_FORMATS, files: List[UploadFile]):
 
 if __name__ == "__main__":
     app.include_router(router)
-
     uvicorn.run(app, host="0.0.0.0", port=8001)
