60d9734e893c2fad50237fe80da7d6ff62d6ea25
LiQeRmAn <evgeniy.bodrov.2005@mail.ru>
2026-04-02 17:32:40 +0300

    Added pdf_service, Added templates for pdf service, implemented in entrypoint.py

--- backend/src/entrypoint.py
+++ backend/src/entrypoint.py
@@ -6,6 +6,8 @@ import uvicorn
 from backend.src.dependencies import get_analyze_service
 from backend.src.services.analyze_service import AnalyzeService
 from backend.src.utils import applogger
+from backend.src.schemas.response_schemas import ApiResponseSchema
+from backend.src.services.pdf_service import PDFService
 
 app = FastAPI()
 router = APIRouter(prefix="/api")
@@ -17,11 +19,17 @@ REPORT_FORMATS = Literal["json", "html", "pdf"]
 async def analyze(url: str, report_format: REPORT_FORMATS, file: UploadFile = File(...),
             analyze_service: AnalyzeService = Depends(get_analyze_service)):
     # TODO валидация url
+    content = await file.read()
+    applogger.debug(type(content))
+    applogger.debug(content)
+    response: ApiResponseSchema = analyze_service.analyze_one(url, content)
     if report_format == "json":
-        content = await file.read()
-        applogger.debug(type(content))
-        applogger.debug(content)
-        return analyze_service.analyze_one(url, content)
+        return response
+    elif report_format == "pdf":
+        pdf_service = PDFService()
+        # передаешь response в твой конвертер pdf и потом возвращаешь сюда файл
+        pdf_content = pdf_service.create_pdf(response)
+        return pdf_content
 
 
 
--- backend/src/services/pdf_service.py
+++ backend/src/services/pdf_service.py
@@ -1,23 +1,24 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
-from backend.src.services.xml_parsing_service import ResponseModel
+from weasyprint.css.validation.properties import direction
+from backend.src.schemas.response_schemas import ApiResponseSchema
 
 class PDFService:
     def __init__(self, template_dir='templates'):
         self.env = Environment(loader=FileSystemLoader(template_dir))
         self.template_name = "report_template.html"
 
-    def create_pdf(self, data: ResponseModel | list[str], output_path: str = None) -> bytes:
-        if isinstance(data, list):
-            template = self.env.get_template('comparison_template.html')
-            html_out = template.render(errors=data)
-        else:
-            template = self.env.get_template('report_template.html')
-            html_out = template.render(
-                code=data.direction_code,
-                name=data.direction_name,
-                year=data.start_year,
-                disciplines=data.disciplines
-            )
+    def create_pdf(self, data: ApiResponseSchema, output_path: str = None) -> bytes:
+        # if isinstance(data, list):
+            # template = self.env.get_template('comparison_template.html')
+            # html_out = template.render(errors=data)
+        # else:
+        template = self.env.get_template('report_template.html')
+        html_out = template.render(
+            direction_code=data.direction_code,
+            direction_name=data.direction_name,
+            start_year=data.start_year,
+            disciplines=data.disciplines
+        )
 
         return HTML(string=html_out).write_pdf(target=output_path)
